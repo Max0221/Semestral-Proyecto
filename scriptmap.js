a98e257f8f1987c433e09a2bed1e3f3c21bfc1cc
Max0221 <80917688+Max0221@users.noreply.github.com>
2021-05-27 15:13:14 -0400

    AAAAAA

--- scriptmap.js
+++ scriptmap.js
@@ -1,11 +1,12 @@
+  
 function iniciarMap(){
-    var coord = {lat:-33.6165597 ,lng: -71.6218349};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
+  var coord = {lat:-33.0336892 ,lng: -71.5331841};
+  var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 10,
+    center: coord
+  });
+  var marker = new google.maps.Marker({
+    position: coord,
+    map: map
+  });
 }
