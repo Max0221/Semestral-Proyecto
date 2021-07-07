01d4edc6e86bb85e31695e1702eafd7c0f6d9367
Max0221 <80917688+Max0221@users.noreply.github.com>
2021-07-07 16:32:01 -0400

    conexion fase de datos y creacion de modelos

--- MiProyecto/core/models.py
+++ MiProyecto/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(primary_key=True, verbose_name='Id categoria')
+    nombreCategoria = models.CharField(max_length=50, verbose_name="Categoria")
+
+    def __str__(self):
+        return self.nombreCategoria 
+
+
+class Obra(models.Model):
+    id_obra = models.IntegerField(primary_key= True, max_length=6 , verbose_name= 'identificador de obra')
+    nombre = models.CharField(max_length=50, verbose_name= 'Nombre de Obra')
+    autor = models.CharField(max_length= 30, verbose_name= 'Autor')
+    descripcion = models.CharField(max_length=50, null=True, blank=True, verbose_name= 'Descripcion de obra')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE )
+
+    def __str__(self):
+        return self.id_obra
+    
