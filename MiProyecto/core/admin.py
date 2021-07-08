885c78f9b942f25f53517ba8d2553111a8340f7c
Max0221 <80917688+Max0221@users.noreply.github.com>
2021-07-08 12:56:18 -0400

    conexion con sqlite, creacion admin

--- MiProyecto/core/admin.py
+++ MiProyecto/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Categoriaa, Arte
 
-# Register your models here.
+admin.site.register(Categoriaa)
+admin.site.register(Arte)
--- MiProyecto/core/models.py
+++ MiProyecto/core/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
-class Categoria(models.Model):
+class Categoriaa(models.Model):
     idCategoria = models.IntegerField(primary_key=True, verbose_name='Id categoria')
-    nombreCategoria = models.CharField(max_length=50, verbose_name="Categoria")
+    nombreCategoriaa = models.CharField(max_length=50, verbose_name="Categoria")
 
     def __str__(self):
-        return self.nombreCategoria 
+        return self.nombreCategoriaa
 
 
-class Obra(models.Model):
-    id_obra = models.IntegerField(primary_key= True, verbose_name= 'identificador de obra')
+class Arte(models.Model):
+    id_arte = models.IntegerField(primary_key= True, verbose_name= 'identificador de obra')
     nombre = models.CharField(max_length=50, verbose_name= 'Nombre de Obra')
     autor = models.CharField(max_length= 30, verbose_name= 'Autor')
     descripcion = models.CharField(max_length=50, null=True, blank=True, verbose_name= 'Descripcion de obra')
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE )
+    categoriaa = models.ForeignKey(Categoriaa, on_delete=models.CASCADE )
 
     def __str__(self):
-        return self.id_obra
+        return self.id_arte
         
     
